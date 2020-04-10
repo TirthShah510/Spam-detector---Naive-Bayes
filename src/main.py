a83de7c36f93ba94c9d769f179e61b3ca77804b1
Parth Patel <patelparth_28@yahoo.com>
2020-04-10 17:28:27 -0400

    Word Frequency||Ham||Spam

--- src/main.py
+++ src/main.py
@@ -4,25 +4,59 @@ import sys
 import re
 import os
 
-path = "../src/train/"
-dir = os.listdir(path)
-dict = {}
 
+def ham_wordcount():
+    path = "../src/train/train_ham/"
+    dir = os.listdir(path)
+    dict_ham = {}
 
-for i in dir:
+    for i in dir:
 
-    file = open(path+i)
+        file = open(path + i)
 
-    l = file.read()
-    l = l.lower()
-    # print(l)
+        l = file.read()
+        l = l.lower()
+        # print(l)
 
-    words = re.split('[^a-zA-Z]+', l)
+        words = re.split('[^a-zA-Z]+', l)
 
-    for word in words:
-        if word not in dict.keys():
-            dict[word] = 1
-        else:
-            dict[word] += 1
+        for word in words:
+            if word not in dict_ham.keys():
+                dict_ham[word] = 1
+            else:
+                dict_ham[word] += 1
 
-print(dict)
+    return dict_ham
+
+def spam_wordcount():
+    path = "../src/train/train_spam/"
+    dir = os.listdir(path)
+    dict_spam = {}
+
+    for i in dir:
+
+        file = open(path + i)
+
+        l = file.read()
+        l = l.lower()
+        # print(l)
+
+        words = re.split('[^a-zA-Z]+', l)
+
+        for word in words:
+            if word not in dict_spam.keys():
+                dict_spam[word] = 1
+            else:
+                dict_spam[word] += 1
+
+    return dict_spam
+
+def main():
+
+    dict_ham = ham_wordcount()
+    dict_spam = spam_wordcount()
+
+    print(dict_ham)
+    print(dict_spam)
+
+main()
