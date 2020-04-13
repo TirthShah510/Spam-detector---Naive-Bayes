b58305205a71b2cf15340f23954dc1aafc79b95c
Parth Patel <patelparth_28@yahoo.com>
2020-04-13 16:02:12 -0400

    Model.txt || Model trained

--- src/main.py
+++ src/main.py
@@ -35,13 +35,15 @@ def wordcount(path):
     for key in sorted(dict.keys()):
         dict_sorted[key] = dict[key]
 
-    return dict_sorted,totalwords
+    return dict_sorted, totalwords
 
-def compute_model(dictspam, dictham):
 
+def merge_dictionary(dictspam, dictham):
     final_dict = {}
     temp_dict = {}
     line_no = 1
+
+    # Creating merged dictionary with "word spam ham" format
     for word in dictspam.keys():
 
         temp = [dictspam[word]]
@@ -53,23 +55,46 @@ def compute_model(dictspam, dictham):
         else:
             temp_dict[word].append(0)
 
-    #Adding those words which are only in Ham
-    for word in  dictham.keys():
+    # Adding those words which are only in Ham
+    for word in dictham.keys():
         temp_dict[word] = [0]
         temp_dict[word].append(dictham[word])
 
-    #Sorting the dictionary
+    # Sorting the dictionary
     for key in sorted(temp_dict.keys()):
         final_dict[key] = temp_dict[key]
 
     return final_dict
 
 
+def compute_model(final_dict, spamwords, hamwords):
+    model = {}
+    vocabulary = len(final_dict)
+    file = open("ans.txt", 'w')
+    i = 1
+    for key in final_dict.keys():
+        # print(str(final_dict[key][0] + 1) + "As" + str((final_dict[key][0] + 1) / (spamwords + vocabulary)))
+        # print(final_dict[key][1])
 
-def main():
+        string = str(i) + "  " + key + "  " + str(final_dict[key][1]) + "  " \
+                 + str((final_dict[key][1] + 1) / (hamwords + vocabulary)) \
+                 + "  " + str(final_dict[key][0]) + "  " \
+                 + str((final_dict[key][0] + 1) / (spamwords + vocabulary)) + "\n"
+        file.write(string)
+
+        # Creating the model(ham,spam)
+        temp_list = [(final_dict[key][1] + 1) / (hamwords + vocabulary),
+                     (final_dict[key][0] + 1) / (spamwords + vocabulary)]
+        model[key] = temp_list
+        i += 1
+    file.close()
+
+    return model
+
+
+def training():
     dict_ham, hamwords = wordcount("../src/train/train_ham/")
     dict_spam, spamwords = wordcount("../src/train/train_spam/")
-
 
     print("Ham")
     f = open("ham.txt", 'w')
@@ -80,11 +105,29 @@ def main():
     for i in dict_spam.keys():
         f.write(i + ":" + str(dict_spam[i]) + "\n")
 
-    model = compute_model(dict_spam, dict_ham)
+    model = merge_dictionary(dict_spam, dict_ham)
     print("Model")
     f = open("model.txt", 'w')
     for i in model.keys():
         f.write(i + ":" + str(model[i]) + "\n")
 
+    model = compute_model(model, spamwords, hamwords)
 
-main()
+    # for word in model.keys():
+    #     print(word+":"+str(model[word])+"\n")
+
+    return model
+
+def testing(model):
+
+
+
+
+
+
+
+
+
+def main():
+    model = training()
+    testing(model)
